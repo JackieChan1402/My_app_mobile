b466c9b3ea01cacb1274b72694ac65a91ffdbce5
JackieChan1402 <vuthanhlong1422003@gmail.com>
2024-09-09 09:26:06 +0700

    first commit

---
+++ app/src/main/java/vn/learn/usth/myapplication/VIewPagerAdapter.java
@@ -0,0 +1,39 @@
+package vn.learn.usth.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class VIewPagerAdapter extends FragmentStateAdapter {
+
+    public VIewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new Tab1Fragment();
+            case 1:
+                return new Tab2Fragment();
+            case 2:
+                return new Tab3Fragment();
+            case 3:
+                return new Tab4Fragment();
+
+            default:
+                return new Tab1Fragment();
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
